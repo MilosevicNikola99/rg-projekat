3cc30be9841d86fefe5e35e657650e888f394aa0
MilosevicNikola99 <nikola9995@yahoo.com>
2022-03-26 00:49:26 +0100

    Face culling i parallax mapping

--- src/main.cpp
+++ src/main.cpp
@@ -28,12 +28,15 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 
 void set_spotLight(Shader& shader);
 
+void renderQuad();
+
 unsigned int loadCubemap(vector<std::string> faces);
 unsigned int loadTexture(const char *path);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+float heightScale = 0.005f;
 
 // camera
 
@@ -168,17 +171,22 @@ int main() {
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_MULTISAMPLE);
+    glEnable(GL_CULL_FACE);
+
+
+
     // build and compile shaders
     // -------------------------
     Shader ourShader("resources/shaders/2.model_lighting.vs", "resources/shaders/2.model_lighting.fs");
     Shader skyboxShader("resources/shaders/skybox.vs","resources/shaders/skybox.fs");
     //Shader tmpShader("resources/shaders/tmp.vs", "resources/shaders/tmp.fs");
     Shader lightingShader("resources/shaders/lightCube.vs","resources/shaders/lightCube.fs");
+    Shader parallaxShader("resources/shaders/parallax_mapping.vs","resources/shaders/parallax_mapping.fs");
 
     // load models
     // -----------
     Model ourModel("resources/objects/LibertyStatue/LibertStatue.obj");
-    Model modelTmp("resources/objects/Wood Table with glasplatte/Wood_Table.obj");
+    Model modelTmp("resources/objects/10421_square_pedastal_iterations-2.obj");
 
     ourModel.SetShaderTextureNamePrefix("material.");
     modelTmp.SetShaderTextureNamePrefix("material.");
@@ -192,18 +200,18 @@ int main() {
             -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
 
             -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-            0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+            0.5f, 0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+            0.5f,  -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
             0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-            0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-            -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+            -0.5f,  -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+            -0.5f, 0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
 
             -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-            -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+            -0.5f,  -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+            -0.5f, 0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
             -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-            -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+            -0.5f, 0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+            -0.5f,  -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
 
             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
             0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
@@ -213,11 +221,11 @@ int main() {
             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
 
             -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-            0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+            0.5f, -0.5f, 0.5f,  0.0f, -1.0f,  0.0f,
+            0.5f, -0.5f,  -0.5f,  0.0f, -1.0f,  0.0f,
             0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-            0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-            -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+            -0.5f, -0.5f,  -0.5f,  0.0f, -1.0f,  0.0f,
+            -0.5f, -0.5f, 0.5f,  0.0f, -1.0f,  0.0f,
 
             -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
             0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
@@ -226,6 +234,7 @@ int main() {
             -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
             -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
     };
+    glFrontFace(GL_CW);
 
 
     float skyboxVertices[] = {
@@ -315,10 +324,18 @@ int main() {
 
 
     unsigned int cubemapTexture = loadCubemap(faces);
+    unsigned int diffuseMap = loadTexture(FileSystem::getPath("resources/textures/Wood_Wall_003_basecolor.jpg").c_str());
+    unsigned int normalMap = loadTexture(FileSystem::getPath("resources/textures/Wood_Wall_003_normal.jpg").c_str());
+    unsigned int heightMap = loadTexture(FileSystem::getPath("resources/textures/Wood_Wall_003_height.png").c_str());
 
 
     skyboxShader.use();
     skyboxShader.setInt("skybox", 0);
+
+    parallaxShader.use();
+    parallaxShader.setInt("diffuseMap", 0);
+    parallaxShader.setInt("normalMap", 1);
+    parallaxShader.setInt("depthMap", 2);
 
 
     PointLight& pointLight = programState->pointLight;
@@ -355,7 +372,7 @@ int main() {
         // don't forget to enable shader before setting uniforms
         ourShader.use();
         pointLight.position=lightPosition;
-        //pointLight.position = glm::vec3(4.0 * cos(currentFrame), 4.0f, 4.0 * sin(currentFrame));
+        pointLight.position = glm::vec3(4.0 * cos(currentFrame), 4.0f, 4.0 * sin(currentFrame));
         ourShader.setVec3("pointLight.position", pointLight.position);
         ourShader.setVec3("pointLight.ambient", pointLight.ambient);
         ourShader.setVec3("pointLight.diffuse", pointLight.diffuse);
@@ -368,6 +385,9 @@ int main() {
 
 
         set_spotLight(ourShader);
+
+        // normal mapping quad
+
 
 
         // view/projection transformations
@@ -386,8 +406,8 @@ int main() {
         model = glm::scale(model, glm::vec3(programState->statueScale));// it's a bit too big for our scene, so scale it down
 #endif
 
-        model = glm::translate(model, glm::vec3(3.0f,0.90f,1.0f)); // translate it down so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(0.6f));// it's a bit too big for our scene, so scale it down
+        model = glm::translate(model, glm::vec3(3.0f,0.419f,1.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(0.3f));// it's a bit too big for our scene, so scale it down
 
         //dodata rotacija oko Oy
         model = glm::rotate(model, glm::radians(currentFrame*50.0f),glm::vec3(0.0f,1.0f,0.0f));// it's a bit too big for our scene, so scale it down
@@ -398,11 +418,38 @@ int main() {
 
     // render the loaded model
         model = glm::mat4(1.0f);
-        model = glm::translate(model,glm::vec3(3.0f,0.0f,1.0f)); // translate it down so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(2.0f));// it's a bit too big for our scene, so scale it down
-
+        model = glm::translate(model,glm::vec3(3.0f,0.419f,1.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(0.005f));// it's a bit too big for our scene, so scale it down
+        model = glm::rotate(model, glm::radians(90.0f),glm::vec3(1.0f,0.0f,0.0f));
         ourShader.setMat4("model", model);
         modelTmp.Draw(ourShader);
+
+
+
+        parallaxShader.use();
+        parallaxShader.setMat4("projection", projection);
+        parallaxShader.setMat4("view", view);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model,glm::vec3(3.0f,0.0f,1.0f));
+        model = glm::scale(model, glm::vec3(0.7f));
+        model = glm::rotate(model, glm::radians(90.0f),glm::normalize(glm::vec3(1.0f,0.0f,.0f)));
+        model = glm::rotate(model, glm::radians(180.0f),glm::normalize(glm::vec3(0.0f,1.0f,.0f)));
+        parallaxShader.setMat4("model", model);
+        parallaxShader.setVec3("viewPos", programState->camera.Position);
+        parallaxShader.setVec3("lightPos",pointLight.position );
+        parallaxShader.setFloat("heightScale", heightScale);
+
+        glDisable(GL_CULL_FACE);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, diffuseMap);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, normalMap);
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, heightMap);
+        renderQuad();
+        glEnable(GL_CULL_FACE);
+
+
 
         //drawing a light cube(light bulb)
         lightingShader.use();
@@ -416,11 +463,14 @@ int main() {
         model = glm::translate(model, lightPosition);
         model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
         lightingShader.setMat4("model", model);
+
         // render the cube
+        glCullFace(GL_BACK);
         glBindVertexArray(cubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
+        glCullFace(GL_FRONT);
         // draw skybox as last
         glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
         skyboxShader.use();
@@ -434,6 +484,7 @@ int main() {
         glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
         glDepthFunc(GL_LESS);
+
 
 
         if (programState->ImGuiEnabled)
@@ -459,6 +510,98 @@ int main() {
     return 0;
 }
 
+// renders a 1x1 quad in NDC with manually calculated tangent vectors
+// ------------------------------------------------------------------
+unsigned int quadVAO = 0;
+unsigned int quadVBO;
+void renderQuad()
+{
+    if (quadVAO == 0)
+    {
+        // positions
+        glm::vec3 pos1(-1.0f,  1.0f, 0.0f);
+        glm::vec3 pos2(-1.0f, -1.0f, 0.0f);
+        glm::vec3 pos3( 1.0f, -1.0f, 0.0f);
+        glm::vec3 pos4( 1.0f,  1.0f, 0.0f);
+        // texture coordinates
+        glm::vec2 uv1(0.0f, 1.0f);
+        glm::vec2 uv2(0.0f, 0.0f);
+        glm::vec2 uv3(1.0f, 0.0f);
+        glm::vec2 uv4(1.0f, 1.0f);
+        // normal vector
+        glm::vec3 nm(0.0f, 0.0f, 1.0f);
+
+        // calculate tangent/bitangent vectors of both triangles
+        glm::vec3 tangent1, bitangent1;
+        glm::vec3 tangent2, bitangent2;
+        // triangle 1
+        // ----------
+        glm::vec3 edge1 = pos2 - pos1;
+        glm::vec3 edge2 = pos3 - pos1;
+        glm::vec2 deltaUV1 = uv2 - uv1;
+        glm::vec2 deltaUV2 = uv3 - uv1;
+
+        float f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+        tangent1.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+        tangent1.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+        tangent1.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+
+        bitangent1.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+        bitangent1.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+        bitangent1.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+
+        // triangle 2
+        // ----------
+        edge1 = pos3 - pos1;
+        edge2 = pos4 - pos1;
+        deltaUV1 = uv3 - uv1;
+        deltaUV2 = uv4 - uv1;
+
+        f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+        tangent2.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+        tangent2.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+        tangent2.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+
+
+        bitangent2.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+        bitangent2.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+        bitangent2.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+
+
+        float quadVertices[] = {
+                // positions            // normal         // texcoords  // tangent                          // bitangent
+                pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+                pos2.x, pos2.y, pos2.z, nm.x, nm.y, nm.z, uv2.x, uv2.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+                pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+
+                pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+                pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+                pos4.x, pos4.y, pos4.z, nm.x, nm.y, nm.z, uv4.x, uv4.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z
+        };
+        // configure plane VAO
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(6 * sizeof(float)));
+        glEnableVertexAttribArray(3);
+        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(8 * sizeof(float)));
+        glEnableVertexAttribArray(4);
+        glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(11 * sizeof(float)));
+    }
+    glBindVertexArray(quadVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
+}
+
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window) {
@@ -482,6 +625,21 @@ void processInput(GLFWwindow *window) {
         lightPosition+=glm::vec3(-1.0f,0.0f,0.0f)*4.0f*deltaTime;
     if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
         lightPosition+=glm::vec3(1.0f,0.0f,0.0f)*4.0f*deltaTime;
+
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+    {
+        if (heightScale > 0.0f)
+            heightScale -= 0.0005f;
+        else
+            heightScale = 0.0f;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+    {
+        if (heightScale < 1.0f)
+            heightScale += 0.0005f;
+        else
+            heightScale = 1.0f;
+    }
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -662,3 +820,5 @@ unsigned int loadCubemap(vector<std::string> faces)
 
     return textureID;
 }
+
+
